8f6552884c719970584c87279fb8cfc4f2b1ff27
sanujkul <sanujkul@gmail.com>
2020-09-18 23:54:29 +0530

    Created func for init grahics

--- 06_Constrains_Sudoku.py
+++ 06_Constrains_Sudoku.py
@@ -8,6 +8,13 @@ win = GraphWin("Sudoku", grid_side*10, grid_side*10);
 
 variablesDict = dict();
 
+
+
+
+
+#numColors has color corresponding to each number from 0 (blank), 1,.,9.
+numColors = ['white','red', 'skyblue', 'yellow', 'green', 'orange', 'pink', 'magenta', 'violet', 'lightgreen']
+
 """
 A1, A2, ..., I8, I9 can be variable names.
 where A--I are for Rows and
@@ -21,9 +28,64 @@ class variable:
     self.domain = domain
     self.row = name[0];
     self.column = name[1];
+    self.color = numColors[value];
 
  # def getVal(self):
   #  return self.value;
+
+
+######################################################################
+######################################################################
+def initGraphics():
+   """
+   This function initializes the board grahics
+
+   """
+   #Coloring the background black
+   win.setBackground(color_rgb(255,255,255));
+   n = 9; #The number of squares per row and column
+   for i in range(1,n+1):
+      for j in range(1,n+1):
+         pt1 = Point((i-1)*grid_side, (j-1)*grid_side)
+         pt2 = Point((i)*grid_side, (j)*grid_side)
+         rect1 = Rectangle(pt1, pt2)
+         rect1.setFill(color_rgb(255,255,255))
+         rect1.draw(win)
+
+   #Writing A.B.C... along a row     
+   x = 10;
+   for row in range(1, n+1): #For every row at column = 10*grid_side;
+      pt1 = Point((x-1)*grid_side, (row-1)*grid_side)
+      pt2 = Point((x)*grid_side, (row)*grid_side)
+      rect1 = Rectangle(pt1, pt2);
+      rect1.setFill(color_rgb(0,0,0))
+      rect1.draw(win)
+
+      val = ascii_uppercase[row-1];
+      label = Text(Point((x-0.5)*grid_side, (row-0.5)*grid_side),val);
+      label.setSize(30)
+      label.setTextColor("white");
+      #print("Writing {} at position ({},{})".format(val,(x-0.5)*grid_side,(row-0.5)*grid_side))
+      label.draw(win)
+      
+   #Writing 1,2,3... along a column     
+   y = 10;
+   for column in range(1, n+1): #For every row at column = 10*grid_side;
+      pt1 = Point((column-1)*grid_side, (y-1)*grid_side)
+      pt2 = Point((column)*grid_side, (y)*grid_side)
+      rect1 = Rectangle(pt1, pt2);
+      rect1.setFill(color_rgb(0,0,0))
+      rect1.draw(win)
+
+      val = column;
+      label = Text(Point((column-0.5)*grid_side, (y-0.5)*grid_side),val);
+      label.setSize(30)
+      label.setTextColor("white");
+      #print("Writing {} at position ({},{})".format(val,(x-0.5)*grid_side,(row-0.5)*grid_side))
+      label.draw(win)
+
+######################################################################
+######################################################################
 
 def initGame():
    """
@@ -39,15 +101,19 @@ def initGame():
    
    for rowNo in ascii_uppercase[0:9]:
       for columnNo in range(1, 10): #From 1 to 9
-         #variableName = "{}{}".format(rowNo,columnNo);
-         print("Added for {}".format(variableName));
+         variableName = "{}{}".format(rowNo,columnNo);
+         #print("Added for {}".format(variableName));
          variablesDict[variableName] = variable(variableName,0,domain); # 0 means blank
    
 
          
+######################################################################
+######################################################################
 
+         
 def main():
    initGame();
+   initGraphics();
    #getMounse() + close(): It wait untilsomeone clicks and doing so closes it
    win.getMouse();
    win.close();
